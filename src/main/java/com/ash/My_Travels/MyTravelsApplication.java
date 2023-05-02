55244b36edc6540ac11c02c583b4b1e0e96fb261
unknown <botoole@BLUME-D75P6S2.blumeglobal.com>
2023-05-01 23:28:34 -0400

    Initial code

---
+++ src/main/java/com/ash/My_Travels/MyTravelsApplication.java
@@ -0,0 +1,13 @@
+package com.ash.My_Travels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTravelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTravelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ash/My_Travels/MyTravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ash.My_Travels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTravelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
